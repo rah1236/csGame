ea555c9c73e7387e3b032cf5d3973dbad2d84b20
rah1236 <rehmatullah4@gmail.com>
2019-04-26 12:57:02 -0500

    redoing everything :(

--- main.swift
+++ main.swift
@@ -1,125 +1,41 @@
 import Foundation
-import Curses
 
-let screen = Screen.shared
 
-class Handler : CursesHandlerProtocol {
-    func interruptHandler() {
-        screen.shutDown()
-        print("Good bye!")
-        exit(0)
-    }
+class Grid {
+    let possibleElements = ["█", " "] //Possible elements that can be held in the grid
+    var gridArray : Array<Array<String>> //An Array that holds Arrays that hold strings
 
-    func windowChangedHandler() {
+    init(width:Int, height:Int){
+        gridArray = Array(repeating: [], count: height)
+        
+        for _ in gridArray{
+            for i in 0..<width{
+                gridArray[i].append((possibleElements.randomElement()!))
+            }
+        }  
     }
 }
 
-enum Direction {
-    case up
-    case down
-    case left
-    case right
-}
-
-class Bullet {
-    var position : Point
-    var direction : Direction
-    var velocity : [Int]
+//class Player {
+  //  var position : Array<Int>
     
 
-    init(position:Point, direction:Direction){
-        self.position = position
-        switch (direction){
-        case Direction.up:
-            self.velocity = [0,-2]
-        case Direction.down:
-            self.velocity = [0,2]
-        case Direction.right:
-            self.velocity = [2,0]
-        case Direction.left:
-            self.velocity = [-2,0]
-        default:
-            self.velocity = [0,0]
-        }
-        self.direction = direction
+//}
+
+
+//grid[4][4] = "░"
+
+var grid = Grid(width:50, height:50)
+
+//system("clear")
+for i in 0..<grid.gridArray.count{
+    for ii in 0..<grid.gridArray[i].count{
+        print(grid.gridArray[i][ii], terminator:"")
     }
-
-    func updatePosition(){
-        position = Point(x:position.x + velocity[0], y:position.y + velocity[1])
-        
-    }
-        
-}
-
-class Player {
-    var position : Point
-    var direction : Direction
-    var velocity : [Int]
-    let window : Window
-    let cursor : Cursor
-    
-    init(playerPos:Point, direction:Direction, velocity:[Int], window:Window){
-      self.position = playerPos
-      self.direction = Direction.right
-      self.velocity = [0,0] //x Velocity, y Velocity
-      self.window = window
-      self.cursor = window.cursor
-    }
-
-    func updatePosition(keyPressed:Key){
-       
-
-
-//        print(keyPressed)
-        
-        switch (keyPressed.keyType) {
-        case .arrowDown:
-            self.position = Point(x:position.x, y:position.y + 1)
-        case .arrowUp:
-            self.position = Point(x:position.x, y:position.y - 1)
-        case .arrowRight:
-            self.position = Point(x:position.x + 1, y:position.y)
-        case .arrowLeft:
-            self.position = Point(x:position.x - 1, y:position.y)
-        default: do {}
-        }
-
-        cursor.position = position
-        print(".")
-        
-        window.refresh()
-        window.clear()
-    }
+    print(terminator:"\n")
 }
 
 
-func main() {
-    // Start up
-    screen.startUp(handler:Handler())
-
-    let mainWindow = screen.window    
     
-    let player = Player(playerPos:Point(x:0, y:0), direction:Direction.right, velocity:[0,0], window:mainWindow)
     
-    let keyboard = Keyboard.shared
-    keyboard.setBufferingDelayed(tenthsOfSecond:1)
-
-    
-    while (true) {
-        let key = keyboard.getKey(window:mainWindow)
-        
-        player.updatePosition(keyPressed:key)
-  
-//        print(key.keyType)
-  
-        mainWindow.refresh()
-    }
-    
-    // Wait forever, or until the user presses ^C
-    screen.wait() //this keeps terminal safe
-}
-
-
-
-main()
 
